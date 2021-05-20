ac352baa6af9d27db7a09c460bf0d956b20873ef
Tyurina Olga <tyurinaolga19@gmail.com>
2021-05-20 13:40:36 +0300

    changed Main.java

--- Main.java
+++ Main.java
@@ -1,24 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        int accountBalance = 100; // rub
-        int topUpBalance = 1300; // rub
-        int amout = topUpBalance / 100;
-        int bonus = 1; // rub
 
-        if (topUpBalance > 1000) ; // rub
+        int accountBalance = 100; // rub
+        int topUpBalance =1400; // rub
+
+        int bonus = topUpBalance / 100; // rub
+        int resultBalance; // rub
+
+        if (topUpBalance > 1000)  // rub
 
         {
-            bonus = 1 * amout; // rub
-
+            resultBalance = accountBalance + topUpBalance + bonus;
+        } else {
+            resultBalance = accountBalance + topUpBalance;
         }
 
-
-        int total = accountBalance + topUpBalance + bonus; // rub
-        System.out.println("Остаток на счете: " + total);
-        // суммы пополнения и тп
-
-        // Условным оператором проверяете превысила ли
-        // сумма пополнения порог и для этих двух разных
-        // сценариев рассчитываете сумму бонуса и выводите
-    }
-}
+        System.out.println("Остаток на счете: " + resultBalance);
